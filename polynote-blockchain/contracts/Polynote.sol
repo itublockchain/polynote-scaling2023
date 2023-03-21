39a8ea7e17b838f0796e9bb66c0a16e1977d44d8
asgarovf <fasgarov2002@gmail.com>
2023-03-21 13:19:10 +0300

    [blockchain] feat: set note id type as string

--- polynote-blockchain/contracts/Polynote.sol
+++ polynote-blockchain/contracts/Polynote.sol
@@ -2,19 +2,25 @@
 pragma solidity ^0.8.9;
 
 contract Polynote {
-    mapping(address => mapping(uint => address[])) sharedAddresses;
+    mapping(address => mapping(string => address[])) sharedAddresses;
 
-    event Shared(address owner, uint notId, address partner);
-    event Unshared(address owner, uint notId, address partner);
+    event Shared(address owner, string notId, address partner);
+    event Unshared(address owner, string notId, address partner);
 
-    function addPartners(uint _notId, address[] memory _partners) public {
+    function addPartners(
+        string memory _notId,
+        address[] memory _partners
+    ) public {
         for (uint256 i = 0; i < _partners.length; ++i) {
             sharedAddresses[msg.sender][_notId].push(_partners[i]);
             emit Shared(msg.sender, _notId, _partners[i]);
         }
     }
 
-    function removePartners(uint _notId, address[] memory _partners) public {
+    function removePartners(
+        string memory _notId,
+        address[] memory _partners
+    ) public {
         for (uint256 i = 0; i < _partners.length; ++i) {
             for (
                 uint256 x = 0;
@@ -31,7 +37,7 @@ contract Polynote {
 
     function isShared(
         address _owner,
-        uint _notId,
+        string memory _notId,
         address _partner
     ) public view returns (bool) {
         bool shared;
@@ -45,7 +51,7 @@ contract Polynote {
 
     function getSharedAddresses(
         address _owner,
-        uint _notId
+        string memory _notId
     ) public view returns (address[] memory) {
         return sharedAddresses[_owner][_notId];
     }
