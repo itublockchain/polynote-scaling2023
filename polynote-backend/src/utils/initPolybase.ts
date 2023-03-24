d2cba5fe402d8d3235784671a0539a2f0f207bdd
asgarovf <fasgarov2002@gmail.com>
2023-03-24 17:14:14 +0300

    [backend] feat: add db namespace to env

--- polynote-backend/src/utils/initPolybase.ts
+++ polynote-backend/src/utils/initPolybase.ts
@@ -2,6 +2,7 @@ import { Polybase } from '@polybase/client';
 import { ethPersonalSign } from '@polybase/eth';
 import { NoteCollection } from 'src/collections/note.collection';
 import { UserCollection } from 'src/collections/user.collection';
+import { CONFIG } from 'src/config';
 
 export const db = new Polybase({
   signer: (data: any) => {
@@ -10,7 +11,7 @@ export const db = new Polybase({
       sig: ethPersonalSign(process.env.PRIVATE_KEY, data),
     };
   },
-  defaultNamespace: 'polynote',
+  defaultNamespace: CONFIG.DB_NAMESPACE,
 });
 
 export const initPolybase = async () => {
