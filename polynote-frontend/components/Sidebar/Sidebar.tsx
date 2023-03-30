d8fbd863dc623a2c5cab34074035606fb783f1c1
asgarovf <fasgarov2002@gmail.com>
2023-03-30 14:52:40 +0300

    [frontend] fix: signature on different chains

--- polynote-frontend/components/Sidebar/Sidebar.tsx
+++ polynote-frontend/components/Sidebar/Sidebar.tsx
@@ -158,16 +158,18 @@ export const Sidebar = ({ createNoteModal }: Props) => {
                           className="bg-neutral-100 hover:bg-neutral-200 dark:bg-MAIN_DARK dark:hover:bg-PURPLE py-1 px-2 rounded-md mb-2 flex items-center cursor-pointer"
                           key={item.sid}
                         >
-                          <div className="shrink-0 mr-2">
-                            <img
-                              alt="notification"
-                              src={item.icon}
-                              className="w-[32px] h-[32px] rounded-md"
-                            />
+                          <div className="flex items-center">
+                            <div className="shrink-0 mr-2">
+                              <img
+                                alt="notification"
+                                src={item.icon}
+                                className="w-[32px] h-[32px] rounded-md"
+                              />
+                            </div>
+                            <span className="text-MAIN_DARK dark:text-PINK">
+                              {item.message}
+                            </span>
                           </div>
-                          <span className="text-MAIN_DARK dark:text-PINK">
-                            {item.message}
-                          </span>
                         </div>
                       ))}
                     </div>
