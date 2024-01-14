c1461ec71515ddf40480e471e7e0c88f641784b0
asgarovf <fasgarov2002@gmail.com>
2024-01-15 01:50:21 +0300

    [frontend] feat: update wallet connect configuration

--- polynote-frontend/config.ts
+++ polynote-frontend/config.ts
@@ -4,6 +4,6 @@ export const CONFIG = {
   APP: "Polynote",
   APP_DESCRIPTION: "Note taking application on Polybase",
   PUSH_CHANNEL_CAIP: "eip155:5:0xA63A810228a180767d3502EF8d21DbF4Da0D6b43",
-  WC_PROJECT_ID: "5f9eb65b68d4fd8c5641c8d09e1ec88e",
+  WC_PROJECT_ID: "13543b39fc73c43252acb941cc642dd3",
   CHAINS: [zksync_testnet],
 };
